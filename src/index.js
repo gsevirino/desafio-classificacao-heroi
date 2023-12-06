29c479bed9cb9424fc4fd6af085317fb7f5efd98
Gislaine Sevirino <gislaine.sevirino@gmail.com>
2023-12-05 21:14:16 -0300

    resolução do desafio de Calculadora de Partidas Rankeadas do Felipão da DIO

--- src/index.js
+++ src/index.js
@@ -1,24 +1,28 @@
-let nome = "Gislaine";
+let nomeDoHeroi = "Gislaine";
+let batalhasVencidas = 100;
+let batalhasPerdidas = 0;
 
-let xp = 12;
+function calcularNivel(vitorias, derrotas) {
+    let saldoVitorias = vitorias - derrotas;
+    let nivel;
 
-let nivel;
-if (xp < 1000) {
-    nivel = "Ferro";
-} else if (xp <= 2000) {
-    nivel = "Bronze";
-} else if (xp <= 5000) {
-    nivel = "Prata";
-} else if (xp <= 6000) {
-    nivel = "Ouro";
-} else if (xp <= 8000) {
-    nivel = "Platina";
-} else if (xp <= 9000) {
-    nivel = "Ascendente";
-} else if (xp <= 10000) {
-    nivel = "Imortal";
-} else {
-    nivel = "Radiante";
+    if (saldoVitorias < 10) {
+        nivel = "Ferro";
+    } else if (saldoVitorias <= 20) {
+        nivel = "Bronze";
+    } else if (saldoVitorias <= 50) {
+        nivel = "Prata";
+    } else if (saldoVitorias <= 80) {
+        nivel = "Ouro";
+    } else if (saldoVitorias <= 90) {
+        nivel = "Diamante";
+    } else if (saldoVitorias <= 100) {
+        nivel = "Lendário";
+    } else {
+        nivel = "Imortal";
+    }
+
+    return `O Herói ${nomeDoHeroi} tem saldo de ${saldoVitorias} vitórias e está no nível ${nivel}`;
 }
 
-console.log(`O Herói de nome ${nome} está no nível de ${nivel}`);
+console.log(calcularNivel(batalhasVencidas, batalhasPerdidas));
